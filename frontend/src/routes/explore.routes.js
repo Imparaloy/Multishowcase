57f7e9ab463d65696447c807a121c63f69cd7327
ThanakritSt <141338370+Thanakrit66070262@users.noreply.github.com>
2025-10-30 23:28:11 +0700

    explore fix

--- frontend/src/routes/explore.routes.js
+++ frontend/src/routes/explore.routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import { attachUserToLocals } from '../middlewares/authenticate.js';
-import { getExplorePage, getExploreFeed } from '../controllers/explore.controller.js';
+import { getExplorePage } from '../controllers/explore.controller.js';
 
 const router = express.Router();
 
 router.get('/explore', attachUserToLocals, getExplorePage);
-router.get('/explore/feed', attachUserToLocals, getExploreFeed);
 
 export default router;
